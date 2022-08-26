3ad87b451c8311c8a16d9c53d301f70fe2e44b61
E-Lop <edmondolopez@gmail.com>
2022-08-26 15:56:53 +0200

    film stampati in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>I miei film</title>
 </head>
 <body>
     <main>
-        <h1>I miei libri</h1>
-
+        <h1>I miei 10 film</h1>
+        @foreach ($movies as $movie)
+            <div class="movie-wrapper">
+                <div class="movie-card">
+                    <div class="title">{{$movie->title}}</div>
+                    <div class="original-title">{{$movie->original_title}}</div>
+                    <div class="country">{{$movie->nationality}}</div>
+                    <div class="date">{{$movie->date}}</div>
+                    <div class="vote">{{$movie->vote}}</div>
+                </div>
+            </div>
+        @endforeach
     </main>
 </body>
 </html>
